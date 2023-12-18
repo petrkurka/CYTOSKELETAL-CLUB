2e760fa2bb3761235d77d74fca325e4e9dc319d4
petrkurka <154247047+petrkurka@users.noreply.github.com>
2023-12-18 16:42:33 +0100

    Update csv2latex.py

--- csv2latex.py
+++ csv2latex.py
@@ -6,7 +6,7 @@ INPUT
 
 OUTPUT
 * structure.tex  // latex formatting info
-* main.tex       // main file containing the latex output
+* main.tex       // main file containing the latex output - compile this file
 
 
 

76b654ee11ee027cfb3d326935acd6c4d0bb9f5d
petrkurka <154247047+petrkurka@users.noreply.github.com>
2023-12-18 16:01:04 +0100

    Update csv2latex.py

--- csv2latex.py
+++ csv2latex.py
@@ -1,10 +1,16 @@
 '''
 This program is designed to convert google forms submitted answers to a latex book of abstracts for cytoskeletal club.
 
+INPUT
+* .csv file from google forms
 
-Petr Kurka
+OUTPUT
+* structure.tex  // latex formatting info
+* main.tex       // main file containing the latex output
 
-UFE
+
+
+Petr Kurka for UFE
 20.06.2023
 '''
 
